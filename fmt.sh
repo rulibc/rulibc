760c74afd569d35f4f0b33e840d9928a7f778835
Yonggang Luo <luoyonggang@gmail.com>
2020-10-15 01:59:42 +0800

    Format more

--- fmt.sh
+++ fmt.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cargo fmt --package rulibc --package crt0 "$@"
+cargo fmt --package rulibc --package crt0 --package crti --package crtn --package crtn ld_so --package crtn ld_so "$@"
